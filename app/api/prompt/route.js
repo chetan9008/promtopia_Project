c5b45d1dc29c14a8d45a7ca30e7179a9cfe9221f
chetan <chetankumar70174577@gmail.com>
2024-05-06 16:43:35 +0530

    xyz

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,3 +1,17 @@
+// import Prompt from "@models/prompt";
+// import { connectToDB } from "./../../../utils/datatbase";
+
+// export const GET = async (request) => {
+//   try {
+//     await connectToDB();
+
+//     const prompts = await Prompt.find({}).populate("creator");
+
+//     return new Response(JSON.stringify(prompts), { status: 200 });
+//   } catch (error) {
+//     return new Response("Failed to fetch all prompts", { status: 500 });
+//   }
+// };
 import Prompt from "@models/prompt";
 import { connectToDB } from "./../../../utils/datatbase";
 
@@ -7,8 +21,13 @@ export const GET = async (request) => {
 
     const prompts = await Prompt.find({}).populate("creator");
 
+    if (!prompts) {
+      throw new Error("No prompts found");
+    }
+
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
+    console.error("Failed to fetch all prompts:", error);
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
